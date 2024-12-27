fedf5f4655e41d74eb683d7fde7ccfce990f168c
Emilio Markgraf <emilio.markgraf@gmail.com>
2024-12-27 14:12:12 +0100

    Day2 Star2

--- src/main/kotlin/emiliomg/aoc/y2024/Day2.kt
+++ src/main/kotlin/emiliomg/aoc/y2024/Day2.kt
@@ -8,11 +8,7 @@ typealias Report = List<Long>
 
 object Day2 : AoCSolution<Int, Long> {
     override fun star1(raw: String): Int {
-        val reports: List<Report> = raw.asProcessedList {
-            this.split(" ").map { it.toLong() }
-        }
-
-        println(reports)
+        val reports: List<Report> = fetchAndPrepareData(raw)
 
         val result = reports
             .mapNotNull { report -> // Remove reports that are not strictly ascending / descending
@@ -38,6 +34,53 @@ object Day2 : AoCSolution<Int, Long> {
     }
 
     override fun star2(raw: String): Long {
-        TODO()
+        val reports: List<Report> = fetchAndPrepareData(raw)
+        val safeReports: Map<String, Boolean> =
+            reports.associate { it.joinToString() to stepThroughReport(it, 0, Direction.UNDECIDED, false) }
+
+        println(safeReports)
+
+        val result = safeReports.count { it.value }.toLong()
+        return result
+    }
+
+    private fun stepThroughReport(report: Report, idx: Int, direction: Direction, dampenerAlreadyEngaged: Boolean): Boolean {
+        if (idx + 1 == report.size) return true
+
+        val (a, b) = report[idx] to report [idx + 1]
+        val hasRightDistance = abs(a - b) in 1..3
+        val (isKeepingDirection: Boolean, newDirection: Direction) = when(direction) {
+            Direction.UNDECIDED -> if (a < b) true to Direction.ASC else true to Direction.DESC
+            Direction.ASC -> if (a < b) true to Direction.ASC else false to Direction.ASC
+            Direction.DESC -> if (a < b) false to Direction.DESC else true to Direction.DESC
+        }
+
+        val isValid = hasRightDistance && isKeepingDirection
+
+        if (isValid) {
+            return stepThroughReport(report, idx + 1, newDirection, dampenerAlreadyEngaged)
+        } else {
+            if (dampenerAlreadyEngaged) return false
+
+            val reportDroppedPreviousElement: Report? = if (idx == 0) null else report.filterIndexed { index, _ -> index != idx - 1 }
+            val reportDroppedCurrentElement: Report = report.filterIndexed { index, _ -> index != idx }
+            val reportDroppedNextElement: Report = report.filterIndexed { index, _ -> index != idx + 1 }
+
+            val isFixedDroppedPrevious =  reportDroppedPreviousElement?.let {
+                stepThroughReport(it, 0, Direction.UNDECIDED, true)
+            } ?: false
+            val isFixedDroppedCurrent = stepThroughReport(reportDroppedCurrentElement, 0, Direction.UNDECIDED, true)
+            val isFixedDroppedNext = stepThroughReport(reportDroppedNextElement, 0, Direction.UNDECIDED, true)
+
+            return isFixedDroppedPrevious || isFixedDroppedCurrent || isFixedDroppedNext
+        }
+    }
+
+    private enum class Direction {
+        ASC, DESC, UNDECIDED;
+    }
+
+    private fun fetchAndPrepareData(raw: String) = raw.asProcessedList {
+        this.split(" ").map { it.toLong() }
     }
 }
--- src/test/kotlin/emiliomg/aoc/y2024/Day2Test.kt
+++ src/test/kotlin/emiliomg/aoc/y2024/Day2Test.kt
@@ -10,7 +10,10 @@ class Day2Test : FunSpec({
             "day2/example.txt" to 2
         ),
         puzzleStar1 = "day2/input.txt" to 371,
-//        examplesStar2 = listOf("day2/example.txt" to 123),
-//        puzzleStar2 = "day2/input.txt" to 123
+        examplesStar2 = listOf(
+            "day2/example.txt" to 4,
+            "day2/edgecases.txt" to 10,
+        ),
+        puzzleStar2 = "day2/input.txt" to 426
     ))
 })
