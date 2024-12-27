b3dbb8b75b9b29477ab475071add56b94c670893
Emilio Markgraf <emilio.markgraf@gmail.com>
2024-12-27 14:14:46 +0100

    Day2, update Star1 to use Star2 code

--- src/main/kotlin/emiliomg/aoc/y2024/Day2.kt
+++ src/main/kotlin/emiliomg/aoc/y2024/Day2.kt
@@ -6,30 +6,15 @@ import kotlin.math.abs
 
 typealias Report = List<Long>
 
-object Day2 : AoCSolution<Int, Long> {
-    override fun star1(raw: String): Int {
+object Day2 : AoCSolution<Long, Long> {
+    override fun star1(raw: String): Long {
         val reports: List<Report> = fetchAndPrepareData(raw)
+        val safeReports: Map<String, Boolean> =
+            reports.associate { it.joinToString() to stepThroughReport(it, 0, Direction.UNDECIDED, true) }
 
-        val result = reports
-            .mapNotNull { report -> // Remove reports that are not strictly ascending / descending
-                val sorted = report.distinct().sorted()
-                if (report == sorted || report == sorted.reversed() ) {
-                    report
-                } else {
-                    null
-                }
-            }
-            .mapNotNull { report -> // Remove reports that are "changing too fast"
-                val changesCorrectly = report
-                    .zipWithNext()
-                    .map { (a, b) ->
-                        abs(a - b) in 1..3
-                    }
-                    .all { it }
-                if (changesCorrectly) report else null
-            }
-            .count()
+        println(safeReports)
 
+        val result = safeReports.count { it.value }.toLong()
         return result
     }
 
